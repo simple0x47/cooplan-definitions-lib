3ea2a3c99e3940dadfb4e365a8577c45c3ebabe7
Gabriel Ami <gabriel@simpleg.eu>
2022-09-17 14:32:21 +0300

    + cooplan's definitions main components

---
+++ src/cooplan/definitions/mod.rs
@@ -0,0 +1,5 @@
+pub mod attribute;
+pub mod category;
+pub mod error;
+pub mod source_attribute;
+pub mod source_category;
---
+++ src/cooplan/definitions/source_category.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+use super::source_attribute::SourceAttribute;
+
+/// Partial or full, input based category.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SourceCategory {
+    pub id: Option<String>,
+    pub parent: Option<String>,
+    pub parent_name: Option<String>,
+    pub name: String,
+    pub selectable_as_last: Option<bool>,
+    pub attributes: Vec<SourceAttribute>,
+}

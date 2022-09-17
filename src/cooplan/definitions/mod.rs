89aff299d55f7acb2e9977c2cfc19fda16fb52ab
Gabriel Ami <gabriel@simpleg.eu>
2022-09-17 14:38:54 +0300

    r move files to root

--- src/cooplan/definitions/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod attribute;
-pub mod category;
-pub mod error;
-pub mod source_attribute;
-pub mod source_category;
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-pub mod cooplan;
+pub mod attribute;
+pub mod category;
+pub mod error;
+pub mod source_attribute;
+pub mod source_category;
